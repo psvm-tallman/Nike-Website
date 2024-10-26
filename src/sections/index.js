e8677bbbd375a9330351f3fafe16773d4669a7a8
Mehul Uttam <mehulsavio@gmail.com>
2024-10-27 00:16:29 +0530

    commit

--- src/sections/index.js
+++
@@ -1,8 +0,0 @@
-import Hero from "./Hero" // eslint-disable-line no-unused-vars
-import PopularProducts from "./PopularProducts" // eslint-disable-line no-unused-vars
-import SuperQuality from "./SuperQuality" // eslint-disable-line no-unused-vars
-import Services from "./Services" // eslint-disable-line no-unused-vars
-import SpecialOffer from "./SpecialOffer" // eslint-disable-line no-unused-vars
-import CustomerReviews from "./CustomerReviews" // eslint-disable-line no-unused-vars
-import Footer from "./Footer" // eslint-disable-line no-unused-vars
-import Subscribe from "./Subscribe" // eslint-disable-line no-unused-vars
