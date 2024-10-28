acc5059a8871624e19044d4b5765126302ba92d4
Mehul Uttam <mehulsavio@gmail.com>
2024-10-28 23:49:49 +0530

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none text-white rounded-full bg-coral-red border-coral-red">
       {label}
 
-       <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
     </button>
   );
 };
